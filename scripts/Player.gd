7f49534df114bb75c7fa9965306eb12e398509d1
Léo Rocha <leopdor@gmail.com>
2021-04-02 20:51:37 -0300

    movendo o personagem

---
+++ scripts/Player.gd
@@ -0,0 +1,25 @@
+extends KinematicBody2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var velocity = Vector2.ZERO
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	
+	if input_vector != Vector2.ZERO:
+		velocity = input_vector
+	else :
+		velocity = Vector2.ZERO
+		
+	move_and_collide(velocity)
