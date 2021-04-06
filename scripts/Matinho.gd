2234a9bb0fdf1e6930355ef39d251ab17b0a78cb
Léo Rocha <leopdor@gmail.com>
2021-04-06 09:43:57 -0300

    ataque fisico com hurtbox and hitbox

--- scripts/Matinho.gd
+++ scripts/Matinho.gd
@@ -1,11 +1,14 @@
 extends Node2D
 
-func _process(delta):
-	if Input.is_action_just_pressed("ui_espace"):
+func create_grass_effect():
 		var EfeitoMatinho = load("res://EfeitoMatinho.tscn")
 		var efeitoMatinho = EfeitoMatinho.instance()
 		efeitoMatinho.global_position = global_position
 		var world = get_tree().current_scene
 		world.add_child(efeitoMatinho)
-		queue_free()
 
+
+
+func _on_Hurtbox_area_entered(area):
+	create_grass_effect()
+	queue_free()
